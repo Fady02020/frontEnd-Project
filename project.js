abcd044bfb9af481973c3f61667eebcb54254f68
Fady02020 <fadysomael@yahoo.com>
2021-10-10 04:47:17 +0200

    third div 'about' done and updated navbar

--- project.js
+++ project.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
     $("#tgl-btn").click(function () {
         $("#navbar").toggleClass("scnd-bg"); 
+        $("#Spread").toggleClass("scnd-bg"); 
+        $("#AboutCorona").toggleClass("scnd-bg"); 
+    });
+});
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 0) {
+            $("nav").removeClass("bg-transparent", function(){
+                $("nav").addClass("bg-white",700);
+            });
+        }
+        if(scroll_pos == 0){
+            $("nav").addClass("bg-transparent",700);
+            $("nav").removeClass("bg-white");
+        }
     });
 });
