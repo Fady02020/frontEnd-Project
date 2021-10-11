52a5409f5dac7fcb3d4517ac67f6516cf82a98a9
Fady02020 <fadysomael@yahoo.com>
2021-10-11 06:09:11 +0200

    fourth div 'symptoms' done and edited navbar

--- project.js
+++ project.js
@@ -11,11 +11,14 @@ $(document).ready(function(){
         scroll_pos = $(this).scrollTop();
         if(scroll_pos > 0) {
             $("nav").removeClass("bg-transparent", function(){
-                $("nav").addClass("bg-white",700);
+                $("nav").addClass("bg-white",500, function(){
+                    $("nav").addClass("shadow");
+                });
             });
         }
         if(scroll_pos == 0){
             $("nav").addClass("bg-transparent",700);
+            $("nav").removeClass("shadow");
             $("nav").removeClass("bg-white");
         }
     });
